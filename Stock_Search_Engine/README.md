744c21aa963e1a30ee38cb85349586d6621a9a4d
unknown <yitianhu@usc.edu>
2022-09-27 17:49:10 -0700

    jar added

---
+++ Stock_Search_Engine/README.md
@@ -0,0 +1,4 @@
+All the source codes are under the src folder. 
+If you wants to use and run the program, please download
+the jar file called Stock_Search_Engine.zip and import to
+desired workspace.
--- Stock_Search_Engine/src/stock_Search_Engine/Launcher.java
+++ Stock_Search_Engine/src/stock_Search_Engine/Launcher.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class Launcher {
 
 	public static void main(String[] args) {
+		//Create a new Engine.
 		Engine myEng = new Engine();
+		//This scanner will be used throughout the execution of the program
+		//to receive command line user inputs
 		Scanner in = new Scanner(System.in);
 		myEng.openFile(in);
 		myEng.openInterface(in);
